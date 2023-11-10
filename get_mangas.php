5892d6db6b54117e25591baad339959c8bdd2c2b
Zakaria DRISSI EL MALIANI <maliani.zakaria@gmail.com>
2023-11-10 21:02:23 +0100

    get data

--- get_mangas.php
+++ get_mangas.php
@@ -8,31 +8,9 @@ if (strtoupper($_SERVER['REQUEST_METHOD']) != 'GET') { // N'accepter que le verb
     die();
 }
 
-//  $body = file_get_contents("php://input");
-//  $object = json_decode($body, true);
-
-// Throw an exception if decoding failed
-//if (!is_array($object)) {
-//    throw new Exception('Failed to decode JSON object');
-//}
-
-$user = $_GET["username"];
-$name = $_GET["name"];
-
 try {
-    $servername = "localhost:3306";
-    $username = "root";
-    $password = "test";
-    $conn = new PDO("mysql:host=$servername;dbname=ifiag", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO users (name, username) VALUES ( '" . $name . "', '" . $user . "')";
-
-    $conn->exec($sql);
-    http_response_code(201);
-    echo json_encode(array(
-        "success" => "The user is successfully added into database"
-    ));
+    http_response_code(200);
+    echo file_get_contents("./data/mangas.json");
 } catch (PDOException $e) {
     http_response_code(500);
     echo json_encode(array(
